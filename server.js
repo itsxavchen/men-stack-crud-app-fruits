e14392cd3bd739d9d37de059406099aee9e418a4
xavierchen <info@xavchen.com>
2024-10-24 11:07:29 +0100

    part 1 finished

---
+++ server.js
@@ -0,0 +1,51 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
+const Fruit = require("./models/fruit.js");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits");
+});
+
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { fruits: allFruits });
+});
+
+app.get("/fruits/new", (req, res) => {
+    res.render("fruits/new.ejs");
+});
+
+app.get("/fruits/:fruitId", (req, res) => {
+  res.send(
+    `This route renders the show page for fruit id: ${req.params.fruitId}!`
+  );
+});
+
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
